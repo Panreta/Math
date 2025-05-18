83a11ae275078805fe7b3de73f3d1fb010659bdd
Panreta <120447314+Panreta@users.noreply.github.com>
2025-05-18 13:20:28 +0800

    some tips collected

---
+++ Tips.for.math/机器学习/s.v.m/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+   # # 可视化训练数据
+        # plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=plt.cm.Paired)#generate 400 points, c gives the diiiferent color
+        # plt.title('Training Data')
+        # plt.figure()
